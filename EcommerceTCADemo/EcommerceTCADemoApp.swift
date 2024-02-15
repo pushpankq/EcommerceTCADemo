4722ab070f8b401e974c374314ca5fde73ffcac2
pushpank <pushpankq@gmail.com>
2024-02-16 03:08:09 +0530

    initial commit

---
+++ EcommerceTCADemo/EcommerceTCADemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceTCADemoApp.swift
+//  EcommerceTCADemo
+//
+//  Created by Pushpank Kumar on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceTCADemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
